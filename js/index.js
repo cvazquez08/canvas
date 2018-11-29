09d4e4c0fdb9b96dbe3593db0e2e7b65d8beccc5
Christopher Vazquez <c_vazquez08@yahoo.com>
2018-11-28 20:13:33 -0500

    collision and gameover

--- js/index.js
+++ js/index.js
@@ -2,14 +2,19 @@ const myCanvas = document.getElementById("my-canvas");
 //ctx has all the drawings methods
 const ctx = myCanvas.getContext("2d");
 
+// global variables:
+let score = 0;
+let isOver = false;
+
 function drawBackground(){
     ctx.fillStyle = "pink";
+    //  1000 === width of canvas 500 === height of canvas (set in index.html when canvas was created)
     ctx.fillRect(0, 0, 1000, 500);
 
     // add some text
     ctx.fillStyle = "green";
     ctx.font = "30px Arial";
-    ctx.fillText("Cool Game :)", 800, 50);
+    ctx.fillText(`Score: ${score}`, 800, 50);
 
 }
 
@@ -69,29 +74,76 @@ function drawingLoop(){
     ctx.clearRect(0, 0, 1000, 500);
 
     drawBackground();
-
+    // change X coordinate -5 everytime it redraws
     fireballX -= 5;
 
+    //once fireball is at -50 (X) draw new fireball at 1000X and random Y position
     if(fireballX < -50){
       fireballX = 1000;
-      fireballY = Math.floor(Math.random() * 500);
+      fireballY = Math.floor(Math.random() * 450);
+      
     }
 
-    // ctx.drawImage(which image, x, y, width, height);
-    ctx.drawImage(fireballImg, fireballX, fireballY, 50, 50);
-
-    // ctx.drawImage(which image, x, y, width, height);
-    ctx.drawImage(supermanImg, supermanX, supermanY, 150, 150);
-
-    //re-draw the whole scene
-
+    drawEverything();
+    if(isOver === false){
+           //re-draw entire canvas
     requestAnimationFrame(function(){
       //sets up a recursive loop (a function that calls itself mutliple times)
       drawingLoop();
     })
+    }
+ 
 
 }
 
+function drawEverything(){
+
+      // ctx.drawImage(which image, x, y, width, height);
+      ctx.drawImage(fireballImg, fireballX, fireballY, 50, 50);
+
+      // ctx.drawImage(which image, x, y, width, height);
+      ctx.drawImage(supermanImg, supermanX, supermanY, 150, 150);
+
+      if(checkCollison(supermanX, supermanY, fireballX, fireballY)){
+        // console.log("ON FIRE!!!!");
+        gameOver();
+      }
+
+      if(fireballX === 0){
+        score++;
+      }
+
+}
+
+function checkCollison(obj1x, obj1y, obj2x, obj2y){
+
+      // supermanY + superman height >= fireballY
+  return obj1y + 150 >= obj2y
+      //  supermanY <= fireballY + fireball height
+      && obj1y <= obj2y + 50
+      //  supermanX + superman width >= fireballX
+      && obj1x + 150 >= obj2x
+      //  supermanX <= fireballx + fireball width
+      && obj1x <= obj2x + 50
+}
+
+function gameOver(){
+    ctx.clearRect(0,0, 1000, 500);
+    drawBackground();
+    
+    const deadSupermanImg = new Image();
+    deadSupermanImg.src = "./images/deadsuperman.png";
+    deadSupermanImg.onload = function(){
+      ctx.drawImage(deadSupermanImg, 480, 300, 150, 150);
+    }
+
+    isOver = true;
+    ctx.font = "bold 70px Monospace";
+    ctx.fillStyle = "red";
+    ctx.fillText("GAME OVER", 400, 225);
+}
+
+// call drawingloop once so it can start, then recursive loop will continue to run
 drawingLoop();
 
 
